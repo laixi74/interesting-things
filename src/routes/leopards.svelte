978de918951d9cbc726ed563c4e94a2ef099a23c
laixi74 <78567259+laixi74@users.noreply.github.com>
2021-03-17 12:39:23 +1300

    all pages have the same style

--- src/routes/leopards.svelte
+++ src/routes/leopards.svelte
@@ -6,7 +6,161 @@
 
 <Nav />
 <Header />
-<!--style one-->
+
+<main>
+    <h2 class="subtitle has-text-centered">Leopards</h2>
+    
+    <p>
+        nenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra v
+            itae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam
+             nulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet nis
+             l. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra vit
+            ae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam n
+            ulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet n
+            isl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncueros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus 
+            viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales nelementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum 
+             eniis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+    
+    </p>
+    
+    <img src="images.jpg" alt="ffds" />
+    
+    <div class="columns is-vcentered ">
+        <div class="column" >
+            <h3>subtitle</h3>
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus 
+            viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum 
+             enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+            sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+            Arcu risus quis varius quam quisque. Quam a
+           dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+           erra aliquet eget sit. Accumsan lacus vel facilisis
+           . Parturient montes nascetur ridiculus mus. Nam a
+           t lectus urna duis. Diam quis enim lobortis sceler
+           isque. Vitae tortor condimentum lacinia quis vel 
+           eros. Consequat nisl v
+           <img class="c-img" src="cat.jpeg" alt="ggg" />
+        </div>
+        <div class="column ">
+            <h3>subtitle</h3>
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue la
+            cus viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliqua
+            m nulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc 
+            aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+            sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+            Arcu risus quis varius quam quisque. Quam a
+           dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+           erra aliquet eget sit. Accumsan lacus vel facilisis
+           . Parturient montes nascetur ridiculus mus. Nam a
+           t lectus urna duis. Diam quis enim lobortis sceler
+           isque. Vitae tortor condimentum lacinia quis vel 
+           eros. Consequat nisl v
+           <img class="c-img" src="img.jpeg" alt="ggg" />
+        </div>
+        <div class="column ">
+            <h3>subtitle</h3>
+          Teros. Consequat nisl vel pretium lectus quam. Viv
+          erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Elementum fa
+          cilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Ar
+          cu non sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+           Arcu risus quis varius quam quisque. Quam a
+          dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+          erra aliquet eget sit. Accumsan lacus vel facilisis
+          . Parturient montes nascetur ridiculus mus. Nam a
+          t lectus urna duis. Diam quis enim lobortis sceler
+          isque. Vitae tortor condimentum lacinia quis vel 
+          eros. Consequat nisl vel pretium lectus quam. Viv
+          erra orci sagittis eu volutpat odio facilis
+          sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+          Arcu risus quis varius quam quisque. Quam a
+         dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+         erra aliquet eget sit. Accumsan lacus vel facilisis
+         . Parturient montes nascetur ridiculus mus. Nam a
+         t lectus urna duis. Diam quis enim lobortis sceler
+         isque. Vitae tortor condimentum lacinia quis vel 
+         eros. Consequat nisl v
+         <img class="c-img" src="lepoard.jpg" alt="ggg" />
+        </div>
+      </div>
+    
+    </main>
+    
+    <Footer />
+    
+    <!--style three-->
+    <style>
+        p{
+            margin-left:20em;
+            margin-right:20em;
+            font-family: 'Pragati Narrow', sans-serif;
+        }
+        img{
+            width:100%;
+            height:40em;
+            margin-bottom:6em;
+            margin-top:9em;
+        }
+        .column{
+            margin-left:4em;
+            margin-right:2em;
+            margin-bottom:3em;
+            font-family: 'Pragati Narrow', sans-serif;
+        }
+        h2{
+            font-family: 'Kalam', cursive;
+            font-size:2em;
+            color:#F18805;
+        }
+        .c-img{
+            margin-top:2em;
+        }
+    </style>
+
+<!--style one
 <main>
     <h2 >Leopards</h2>
     <div class="container">
@@ -48,8 +202,9 @@
     </p>
 </div>
 </main>
+-->
 
-<!--style one-->
+<!--style one
 <style>
     img{
         height:50em;
@@ -67,3 +222,4 @@
         margin-left:31em;
     }
 </style>
+-->
--- src/routes/tigers.svelte
+++ src/routes/tigers.svelte
@@ -5,10 +5,163 @@
 </script>
 
 
-
-<Header />
 <Nav />
+<Header />
 
+<main>
+    <h2 class="subtitle has-text-centered">Tigers</h2>
+    
+    <p>
+        nenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra v
+            itae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam
+             nulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet nis
+             l. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra vit
+            ae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam n
+            ulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet n
+            isl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncueros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus 
+            viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales nelementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum 
+             eniis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+    
+    </p>
+    
+    <img src="images.jpg" alt="ffds" />
+    
+    <div class="columns is-vcentered ">
+        <div class="column" >
+            <h3>subtitle</h3>
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus 
+            viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt
+             tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Arcu non so
+             dales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum 
+             enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+            sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+            Arcu risus quis varius quam quisque. Quam a
+           dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+           erra aliquet eget sit. Accumsan lacus vel facilisis
+           . Parturient montes nascetur ridiculus mus. Nam a
+           t lectus urna duis. Diam quis enim lobortis sceler
+           isque. Vitae tortor condimentum lacinia quis vel 
+           eros. Consequat nisl v
+           <img class="c-img" src="cat.jpeg" alt="ggg" />
+        </div>
+        <div class="column ">
+            <h3>subtitle</h3>
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue la
+            cus viverra vitae congue. Elementum facilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliqua
+            m nulla facilisi cras fermentum odio eu feugiat. Arcu non sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc 
+            aliquet bibendum enim facilisis gravida. Arcu risus quis varius quam quisque. Quam a
+            dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+            erra aliquet eget sit. Accumsan lacus vel facilisis
+            . Parturient montes nascetur ridiculus mus. Nam a
+            t lectus urna duis. Diam quis enim lobortis sceler
+            isque. Vitae tortor condimentum lacinia quis vel 
+            eros. Consequat nisl vel pretium lectus quam. Viv
+            erra orci sagittis eu volutpat odio facilis
+            sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+            Arcu risus quis varius quam quisque. Quam a
+           dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+           erra aliquet eget sit. Accumsan lacus vel facilisis
+           . Parturient montes nascetur ridiculus mus. Nam a
+           t lectus urna duis. Diam quis enim lobortis sceler
+           isque. Vitae tortor condimentum lacinia quis vel 
+           eros. Consequat nisl v
+           <img class="c-img" src="img.jpeg" alt="ggg" />
+        </div>
+        <div class="column ">
+            <h3>subtitle</h3>
+          Teros. Consequat nisl vel pretium lectus quam. Viv
+          erra orci sagittis eu volutpat odio facilisis. Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Elementum fa
+          cilisis leo vel fringilla est ullamcorper eget. Tincidunt tortor aliquam nulla facilisi cras fermentum odio eu feugiat. Ar
+          cu non sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+           Arcu risus quis varius quam quisque. Quam a
+          dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+          erra aliquet eget sit. Accumsan lacus vel facilisis
+          . Parturient montes nascetur ridiculus mus. Nam a
+          t lectus urna duis. Diam quis enim lobortis sceler
+          isque. Vitae tortor condimentum lacinia quis vel 
+          eros. Consequat nisl vel pretium lectus quam. Viv
+          erra orci sagittis eu volutpat odio facilis
+          sodales neque sodales ut etiam sit amet nisl. Viverra ipsum nunc aliquet bibendum enim facilisis gravida.
+          Arcu risus quis varius quam quisque. Quam a
+         dipiscing vitae proin sagittis nisl rhoncunenatis a. Sed enim ut sem viv
+         erra aliquet eget sit. Accumsan lacus vel facilisis
+         . Parturient montes nascetur ridiculus mus. Nam a
+         t lectus urna duis. Diam quis enim lobortis sceler
+         isque. Vitae tortor condimentum lacinia quis vel 
+         eros. Consequat nisl v
+         <img class="c-img" src="lepoard.jpg" alt="ggg" />
+        </div>
+      </div>
+    
+    </main>
+    
+    <Footer />
+    
+    <!--style three-->
+    <style>
+        p{
+            margin-left:20em;
+            margin-right:20em;
+            font-family: 'Pragati Narrow', sans-serif;
+        }
+        img{
+            width:100%;
+            height:40em;
+            margin-bottom:6em;
+            margin-top:9em;
+        }
+        .column{
+            margin-left:4em;
+            margin-right:2em;
+            margin-bottom:3em;
+            font-family: 'Pragati Narrow', sans-serif;
+        }
+        h2{
+            font-family: 'Kalam', cursive;
+            font-size:2em;
+            color:#F18805;
+        }
+        .c-img{
+            margin-top:2em;
+        }
+    </style>
+
+<!--style two
 <main>
     <h2 class="subtitle has-text-centered">Tiger</h2> 
 
@@ -143,10 +296,11 @@
         <div class="column "><img src="cat.jpeg" alt="cat"/></div>
     </div>
 </main>
+-->
 
 
-<Footer />
 
+<!--style two
 <style>
     img{
         width:30em;
@@ -186,3 +340,4 @@
         margin-bottom:6em;
     }
 </style> 
+-->
